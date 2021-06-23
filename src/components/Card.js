83c2f21c59de446deaf7abe583d40269f39ad984
Marie Otaki <marieotaki@Maries-MacBook-Air.local>
2021-06-23 07:37:21 -0700

    add styles to the type lists

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,19 +3,31 @@ import React from 'react'
 //utils
 import convertIntoThreeDigit from "../utils/convertIntoThreeDigit"
 
-const Card = ({list}) => {
+const Card = ({ list }) => {
+    //generate type lists
+    const generateTypeList = (typeList) => {
+        const lists = typeList.map((val, index) => <span className='type-list' key={index}>{val}</span>)
+
+        return lists;
+    }
+
+
     return (
       <div className="card">
         <div className="card-inner">
           <div className="card-profile-photo">
             <img
-              src={`${process.env.PUBLIC_URL}/thumbnails/${convertIntoThreeDigit(list.id)}.png`}
+              src={`${
+                process.env.PUBLIC_URL
+              }/thumbnails/${convertIntoThreeDigit(list.id)}.png`}
               alt="profile"
             />
           </div>
           <div className="card-profile-info">
             <div className="card-profile-name">{`Name: ${list.name.english}`}</div>
-            <div className="card-profile-type">{`Type: ${list.type}` }</div>
+            <div className="card-profile-type">
+              Type: {generateTypeList(list.type)}
+            </div>
           </div>
         </div>
       </div>
